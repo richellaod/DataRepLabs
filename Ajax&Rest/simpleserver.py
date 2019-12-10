492ac89311d1b33d3a8124229c2057080fad9bc2
richellaod <36302856+richellaod@users.noreply.github.com>
2019-12-10 20:09:53 +0000

    Create simpleserver.py

---
+++ Ajax&Rest/simpleserver.py
@@ -0,0 +1,21 @@
+#!flask/bin/python 
+
+from flask import Flask 
+
+ 
+
+app = Flask(__name__, 
+
+            static_url_path='',
+
+             static_folder='../') 
+
+def index():
+
+	return"Hello, World!" 
+
+ 
+
+if __name__ == '__main__' :     
+
+	app.run(debug= True)
